dc741e1d87ab074918e9e8e779e3536e3424394a
suhail <mr.suhailsulu@gmail.com>
2026-03-24 14:44:48 +0530

    admin page default

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,4 +1,4 @@
-export function AdminDashboardPage() {
+export default function AdminDashboardPage() {
     return (
         <div className="p-4">
             <h1 className="text-2xl font-bold mb-4">Admin Dashboard</h1>

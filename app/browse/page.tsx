4726ca6230543cb3c2b127afb1af368955d3c9d6
suhail <mr.suhailsulu@gmail.com>
2026-03-24 14:47:07 +0530

    deafult in browse page

--- app/browse/page.tsx
+++ app/browse/page.tsx
@@ -1,4 +1,4 @@
-export function BrowsePage() {
+export default function BrowsePage() {
     return (
         <div className="p-4">
             <h1 className="text-2xl font-bold mb-4">Browse Assets</h1>
